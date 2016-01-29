0848d1c551fe5471c050de0b5d61bf6413602fa9
beimarhuarachi <beimar020@gmail.com>
2016-01-29 01:23:06 -0400

    modificado las rutas para la pagina de inicio

---
+++ complejoapp/app/visitante/inicio/inicio.controller.js
@@ -0,0 +1,15 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('complejo.visitante')
+		.controller('InicioController', InicioController);
+
+	InicioController.$inject = ['$log', '$scope'];
+
+	function InicioController($log, $scope) {
+		var vm = this;
+		
+	}
+
+})();
